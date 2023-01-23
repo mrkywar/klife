1ee89e7292206d6bf89e569f23a506b9b4c327eb
mykywar <mr.kywar@gmail.com>
2023-01-23 23:49:41 +0100

    [WIP] Missing Implementation

--- modules/php/Game/Card/Category/Special/Special.php
+++ modules/php/Game/Card/Category/Special/Special.php
@@ -3,13 +3,14 @@
 namespace SmileLife\Game\Card\Category\Special;
 
 use SmileLife\Game\Card\Core\Card;
+use SmileLife\Game\Card\Effect\CardEffectInterface;
 
 /**
  * Description of Special
  *
  * @author Mr_Kywar mr_kywar@gmail.com
  */
-abstract class Special extends Card {
+abstract class Special extends Card implements CardEffectInterface{
 
     
 
